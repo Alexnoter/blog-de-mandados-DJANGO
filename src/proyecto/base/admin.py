9bb03d88008b408e2cd5d6229276a175aedc1472
AlexanderV <alexandervelarde2511@gmail.com>
2023-04-16 16:11:30 -0400

    Creamos una tabla y luego hicimos las migraciones y luego migramos a la pagina con comandos y visualizamos en admin importando la Tarea

--- src/proyecto/base/admin.py
+++ src/proyecto/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Tarea
+
+
 
 # Register your models here.
+
+#con esto hacemos que se visualice nuestra App de tarea en pantalla
+admin.site.register(Tarea)
--- src/proyecto/base/models.py
+++ src/proyecto/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+# tabla para usuario
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+class Tarea(models.Model):
+    usuario = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True)  # este campo puede quedar en blanco
+    titulo = models.CharField(max_length=200)
+    descripcion = models.TextField(null=True,
+                                   blank=True)
+    completo = models.BooleanField(default=False)
+    creado = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+    class Meta:
+        ordering = ['completo']
