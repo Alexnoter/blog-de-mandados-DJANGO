e26a99ff132a14cb8acaa26be3d699409ca338ef
AlexanderV <alexandervelarde2511@gmail.com>
2023-04-19 05:24:59 -0400

    hicimos el apartado de login haciendo que el usuario pueda logearse y salir de la sesion

--- src/proyecto/base/urls.py
+++ src/proyecto/base/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea,EliminarTarea
+from .views import ListaPendientes, DetalleTarea, CrearTarea, EditarTarea, EliminarTarea, Logueo
+from django.contrib.auth.views import LogoutView
 
 #urls path no puede mostrar clases , el metodo es as_view() para que si pueda
 urlpatterns = [path('', ListaPendientes.as_view(), name='tareas'),
+               path('login/', Logueo.as_view(), name='login'),
+               path('logout/', LogoutView.as_view(next_page='login'), name='logout'), #ese next es para direccionar
                path('tarea/<int:pk>', DetalleTarea.as_view(), name='tarea'),
                path('crear-tarea/', CrearTarea.as_view(), name='crear-tarea'),
                path('editar-tarea/<int:pk>', EditarTarea.as_view(), name='editar-tarea'),
--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -2,12 +2,21 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from .models import Tarea
 
 
 
 # Create your views here.
+
+class Logueo(LoginView):
+    template_name = 'base/login.html'
+    field = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tareas')
 
 
 #listaview requiere 2 cosas un model y un qriser
