2b7a2ad1e06c1f2e4ea4a713f684f5ba0e60ef24
AlexanderV <alexandervelarde2511@gmail.com>
2023-04-20 09:58:14 -0400

    Restingir acceso solo a personas logeadas

--- src/proyecto/base/views.py
+++ src/proyecto/base/views.py
@@ -3,6 +3,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin   #restingir , estableces que clase de usuario sera
 from django.urls import reverse_lazy
 from .models import Tarea
 
@@ -20,29 +21,29 @@ class Logueo(LoginView):
 
 
 #listaview requiere 2 cosas un model y un qriser
-class ListaPendientes(ListView):
+class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = 'tareas'
 
-class DetalleTarea(DetailView):
+class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
     context_object_name = 'tarea'
     template_name = 'base/tarea.html' # con esto decimos que me reconosca que ahora sera tarea.html y no tarea_detail
 
 
-class CrearTarea(CreateView):
+class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
     fields = '__all__' #con esto cargamos todos los datos del formulario de los modelos todos los datos otra manera de cargar es []
     success_url = reverse_lazy('tareas')
 
 
-class EditarTarea(UpdateView):
+class EditarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
     fields = '__all__'
     success_url = reverse_lazy('tareas')
 
 
-class EliminarTarea(DeleteView):
+class EliminarTarea(LoginRequiredMixin, DeleteView):
     model = Tarea
     context_object_name = 'tarea'
     success_url = reverse_lazy('tareas')
